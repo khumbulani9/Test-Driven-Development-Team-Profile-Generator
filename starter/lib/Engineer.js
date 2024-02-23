1db578df5e00b0444ffab4db49ddbf453d304167
khumbulani9 <mduduzi1234gmail.com>
2024-02-23 21:24:08 +0000

    engineer.js file complete and inputted with code

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+// Define the Engineer class, extending the Employee class
+class Engineer extends Employee {
+  // Constructor to initialize Engineer-specific properties along with common properties from Employee
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  // Override the getRole method to return Engineer
+  getRole() {
+    return "Engineer";
+  }
+
+  // Method specific to Engineer to get the GitHub username
+  getGithub() {
+    return this.github;
+  }
+}
+
+// Export the Engineer class
+module.exports = Engineer;
