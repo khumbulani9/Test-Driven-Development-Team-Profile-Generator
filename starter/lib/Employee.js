2264f318d22c083b7bdb6a89f3c91334a3d7530b
khumbulani9 <mduduzi1234gmail.com>
2024-02-23 21:04:40 +0000

    updating the employee.js file

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,9 @@
 // TODO: Write code to define and export the Employee class
+// Define the Employee class, which serves as the parent class for manager, engineer, and intern 
+class Employee {
+ // constructor to initialize the common properties for all employees
+ construct(name, id, email) {
+  this,name = name;
+  this.id = id;
+ }
+}
